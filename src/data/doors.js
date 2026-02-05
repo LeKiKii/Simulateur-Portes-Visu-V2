36ba6b212a1b970f23a6fcdd5461aae18802d72f
LeKiKii <132994384+LeKiKii@users.noreply.github.com>
2026-02-05 08:41:26 +0000

    Lien images portes

--- src/data/doors.js
+++ src/data/doors.js
@@ -6,19 +6,25 @@ export const DOORS = [
     id: "b1",
     name: "Chêne Classique",
     category: "Bois",
-    imageSrc: "https://placehold.co/300x600/8B4513/FFFFFF?text=Bois+Classique",
+    imageSrc: "public/images/doors/31-colombelles-duomixte-zilten-porte-entree-chˆne-naturel-300.jpg",
   },
   {
     id: "b2",
     name: "Pin Naturel",
     category: "Bois",
-    imageSrc: "https://placehold.co/300x600/D2691E/FFFFFF?text=Pin+Naturel",
+    imageSrc: "public/images/doors/31-antalya-duo-mixte-zilten-porte-entree-chene-blanchi-300.jpg",
   },
   {
     id: "b3",
     name: "Acajou Prestige",
     category: "Bois",
-    imageSrc: "https://placehold.co/300x600/A0522D/FFFFFF?text=Acajou+Prestige",
+    imageSrc: "public/images/doors/00-zilten-porte-entree-nativ2-miel-300.jpg",
+  },
+  {
+    id: "b4",
+    name: "Acajou Prestige",
+    category: "Bois",
+    imageSrc: "public/images/doors/00-lutece-bois-zilten-porte-entree-bleu-5010-montecarlo-300.jpg",
   },
 
   // PVC
@@ -26,19 +32,25 @@ export const DOORS = [
     id: "p1",
     name: "PVC Blanc Simple",
     category: "PVC",
-    imageSrc: "https://placehold.co/300x600/F0F0F0/000000?text=PVC+Blanc",
+    imageSrc: "public/images/doors/21-tracy1-pvc-zilten-porte-entree-blanc-300.jpg",
   },
   {
     id: "p2",
     name: "PVC Vitré",
     category: "PVC",
-    imageSrc: "https://placehold.co/300x600/E0E0E0/000000?text=PVC+Vitré",
+    imageSrc: "public/images/doors/00-vega2-pvc-zilten-porte-entree-3004-300.jpg",
   },
   {
     id: "p3",
     name: "PVC Gris Anthracite",
     category: "PVC",
-    imageSrc: "https://placehold.co/300x600/708090/FFFFFF?text=PVC+Gris",
+    imageSrc: "public/images/doors/00-kellys5-pvc-zilten-porte-entree-blanc-300.jpg",
+  },
+  {
+    id: "p4",
+    name: "PVC Gris Anthracite",
+    category: "PVC",
+    imageSrc: "public/images/doors/00-holly3-pvc-zilten-porte-entree-blanc-72.jpg",
   },
 
   // Aluminium
@@ -46,32 +58,104 @@ export const DOORS = [
     id: "a1",
     name: "Alu Noir Mat",
     category: "Aluminium",
-    imageSrc: "https://placehold.co/300x600/1A1A1A/FFFFFF?text=Alu+Noir",
+    imageSrc: "public/images/doors/00-cedousa-20-acier-zilten-porte-entree-bleu-2700-H2250-300.jpg",
   },
   {
     id: "a2",
     name: "Alu Gris Perle",
     category: "Aluminium",
-    imageSrc: "https://placehold.co/300x600/B0C4DE/000000?text=Alu+Gris",
+    imageSrc: "public/images/doors/00-arkad-alu-confort-zilten-porte-entree-blanc-9016-txt-300.jpg",
   },
   {
     id: "a3",
     name: "Alu Design",
     category: "Aluminium",
-    imageSrc: "https://placehold.co/300x600/2F4F4F/FFFFFF?text=Alu+Design",
+    imageSrc: "public/images/doors/00-camas-alu-confort-zilten-porte-entree-bleu-2700-texture-300.jpg",
   },
-
+  {
+    id: "a4",
+    name: "Alu Design",
+    category: "Aluminium",
+    imageSrc: "public/images/doors/00-volt-alu-confort-zilten-porte-entree-bleu-2700-txt-300.jpg",
+  },
+  {
+    id: "a5",
+    name: "Alu Design",
+    category: "Aluminium",
+    imageSrc: "public/images/doors/00-soffi-1-alu-confort-zilten-porte-entree-Bleu-baku.jpg",
+  },
+  {
+    id: "a6",
+    name: "Alu Design",
+    category: "Aluminium",
+    imageSrc: "public/images/doors/00-nativ21-fusion-zilten-porte-entree-sugi-ban-L1100-300.jpg",
+  },
+  {
+    id: "a7",
+    name: "Alu Design",
+    category: "Aluminium",
+    imageSrc: "public/images/doors/00-line 3-alu-zilten-porte-entree-bleu-2700t-300.jpg",
+  },
+  {
+    id: "a8",
+    name: "Alu Design",
+    category: "Aluminium",
+    imageSrc: "public/images/doors/00-K5-FAKTORY-alu-ULTIME-zilten-porte-entree-bleu-5003.jpg",
+  },
+  
   // Acier
   {
     id: "ac1",
     name: "Acier Blindé",
     category: "Acier",
-    imageSrc: "/images/doors/00-akita-20-acier-zilten-porte-entree-gris-7016-texture-300.jpg",
+    imageSrc: "public/images/doors/00-akita-20-acier-zilten-porte-entree-gris-7016-texture-300.jpg",
   },
   {
     id: "ac2",
     name: "Acier Industriel",
     category: "Acier",
-    imageSrc: "https://placehold.co/300x600/555555/FFFFFF?text=Acier+Indus",
+    imageSrc: "public/images/doors/00-amaya-20-acier-zilten-porte-entree-noir-2100-300.jpg",
+  },
+  {
+    id: "ac3",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-cedousa-20-acier-zilten-porte-entree-bleu-2700-H2250-300.jpg",
+  },
+  {
+    id: "ac4",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-charley-1-acier-zilten-porte-entree-gris-7047-300.jpg",
+  },
+  {
+    id: "ac5",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-amaya-20-acier-zilten-porte-entree-noir-2100-300.jpg",
+  },
+  {
+    id: "ac6",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-segovia-1-acier-zilten-porte-entree-gris-9006-L900.jpg",
+  },
+  {
+    id: "ac7",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-saori-acier-zilten-porte-entree-noir-2100-texture-300.jpg",
+  },
+  {
+    id: "ac8",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-nagano-acier-zilten-porte-entree-blanc-7047-satine-poignee-300.jpg",
+  },
+  {
+    id: "ac9",
+    name: "Acier Industriel",
+    category: "Acier",
+    imageSrc: "public/images/doors/00-isami-2-acier-zilten-porte-entree-bleu-2700-texture.jpg",
   },
 ];
